acf3f4abff5e31932574e9ef5a2192d2484bda82
psmdevgit <mis@pothysswarnamahal.com>
2025-08-19 16:22:11 +0530

    change the ui bugs

--- src/app/Models/add-models/page.tsx
+++ src/app/Models/add-models/page.tsx
@@ -142,7 +142,7 @@ const StoneDetailsModal = ({
 
 // Main Component
 const AddJewelryModel = () => {
-  const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL || "https://erp-server-r9wh.onrender.com" ;
+  const apiBaseUrl = "https://erp-server-r9wh.onrender.com" ;
 
   
   // const apiBaseUrl = process.env.API_URL || "http://localhost:5001" ;
