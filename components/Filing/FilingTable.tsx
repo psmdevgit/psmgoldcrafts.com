83f8dbaae275ccda5b72af186eef0c71d7544174
psmdevgit <mis@pothysswarnamahal.com>
2025-08-25 18:31:50 +0530

    edit disable

--- components/Filing/FilingTable.tsx
+++ components/Filing/FilingTable.tsx
@@ -492,6 +492,8 @@ console.log("Deals State:", deals);
                                     </button>
                                   </Link>
 
+
+                                  {deal.status?.toLowerCase() !== 'finished' ? (
                                     <Link href={`/Departments/Filing/filing_received_details?filingId=${deal.id}`} passHref>
                                       <button
                                         type="button"
@@ -511,6 +513,28 @@ console.log("Deals State:", deals);
                                         <i className="fa-sharp fa-light fa-pen"></i>
                                       </button>
                                     </Link>
+                                          ) : (
+                                              <button
+                                                type="button"
+                                                className="table__icon edit"
+                                                style={{
+                                                  display: 'inline-block',
+                                                  backgroundColor: 'gray',
+                                                  color: 'white',
+                                                  borderRadius: '4px',
+                                                  padding: '5px',
+                                                  textDecoration: 'none',
+                                                  border: 'none',
+                                                  cursor: 'not-allowed',
+                                                  opacity: 0.6,
+                                                }}
+                                                disabled
+                                                title="Cannot edit finished items"
+                                              >
+                                                <i className="fa-sharp fa-light fa-pen"></i>
+                                              </button>
+                                            )}
+
                                  
 
                                   <button
