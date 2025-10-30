4ed7f59b146f8e1102a0736b0f7efac568153764
psmdevgit <mis@pothysswarnamahal.com>
2025-10-30 10:15:58 +0530

    Update page.tsx

--- src/app/Departments/Waxing/page.tsx
+++ src/app/Departments/Waxing/page.tsx
@@ -12,8 +12,9 @@ import { Portal } from "@radix-ui/react-portal"; // ✅
 
 //const apiUrl = "http://localhost:5001";
 
+const apiUrl = "https://erp-server-r9wh.onrender.com";
 
-const apiUrl = "https://kalash.app";
+// const apiUrl = "https://kalash.app";
 // Interfaces
 interface InventoryItem {
   id: string;
