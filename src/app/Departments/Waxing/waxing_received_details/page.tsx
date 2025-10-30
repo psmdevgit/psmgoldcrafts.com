efee2a456e5b4d7acd2dc1147cd71b9d68aca2db
psmdevgit <mis@pothysswarnamahal.com>
2025-10-30 10:16:21 +0530

    Update page.tsx

--- src/app/Departments/Waxing/waxing_received_details/page.tsx
+++ src/app/Departments/Waxing/waxing_received_details/page.tsx
@@ -6,7 +6,9 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { z } from 'zod';
 
-const apiBaseUrl = "https://kalash.app";
+// const apiBaseUrl = "https://kalash.app";
+
+const apiBaseUrl = "https://erp-server-r9wh.onrender.com";
 
 
 
